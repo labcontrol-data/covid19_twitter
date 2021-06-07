643c18b77108979adc1a68cad0be673d14213b87
Labcontrol <61364361+labcontrol-data@users.noreply.github.com>
2021-06-07 09:20:45 -0300

    Update mainCode.py

--- source-code/mainCode.py
+++ source-code/mainCode.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed Apr  28 17:55:39 2021
+Created on Wed Jun 8 17:55:39 2021
 
 @author: vargas
 
@@ -45,7 +45,7 @@ linkDB=[];
 # variable `linkDB' is a database containing all dates under study
 dateDB=[];
 d1 = datetime.date(2020,3,1)    # start date: 2020-3-22 
-d2 = datetime.date(2021,5,15)    # end date: 2021-5-15
+d2 = datetime.date(2021,6,2)    # end date: 2021-6-2
 diff = d2 - d1
 for i in range(diff.days + 1):
     date_str = (d1 + datetime.timedelta(i)).isoformat()
@@ -65,7 +65,7 @@ for d in range(0,len(linkDB)):
     df = pd.read_csv(url, error_bad_lines=True)
     ocurrences = df.ocurrences.sum()
     john = pd.Series(data=[date_str,ocurrences],
-                     index=dictDB.columns)
+                      index=dictDB.columns)
     dictDB = dictDB.append(john,ignore_index=True)
 
 
@@ -85,3 +85,50 @@ dictDB.to_csv('databaseNegativeFinal.csv', index = False)
 listNegativeWords  = list(dict.fromkeys(df['words']))
 print('\n Negative words in the database:\n')
 print(listNegativeWords)
+
+
+###########################################################################
+# Procedure to create a database with the number of tweets in English
+###########################################################################
+
+def insertStrID(datex):
+    texto1 = 'https://raw.githubusercontent.com/labcontrol-data/covid19_twitter/main/dailies/'
+    texto2 = '_tweets_ID.tsv'
+    return texto1 + datex + '/' + datex + texto2
+
+
+# variable `linkDB' is a database containing all urls
+linkDB=[]; 
+
+# variable `linkDB' is a database containing all dates under study
+dateDB=[];
+d1 = datetime.date(2020,3,1)     # start date: 2020-3-22
+d2 = datetime.date(2021,6,2)     # end date: 2021-6-2
+diff = d2 - d1
+for i in range(diff.days + 1):
+    date_str = (d1 + datetime.timedelta(i)).isoformat()
+    linkDB.append(insertStrID(date_str))  #creates the address link database
+    dateDB.append(date_str)             #creates the database of dates
+    #print( insertStrID(date_str) )
+
+# creates a dictionary structure to record the number of  tweets in English
+# the element 'rate' informs us what is the share of English among all idioms
+dictQuan = pd.DataFrame(columns=['date', 'ocurrences', 'rate'])
+
+
+for d in range(0,len(linkDB)):    
+    date_str = dateDB[d]
+    print('Day covered_ID: ', date_str )
+    fileN = linkDB[d] 
+    df = pd.read_csv(fileN,sep='\t')
+
+    countEnglish = len(df[df['lang'] == "en"])
+
+    ocurrences = countEnglish
+    ratex = ocurrences/len(df)
+    john = pd.Series(data=[date_str,ocurrences, ratex],
+                        index=dictQuan.columns)
+    dictQuan = dictQuan.append(john,ignore_index=True)
+
+
+dictQuan.to_csv('databaseOcurrencesEnglishFinal.csv', index = False)
